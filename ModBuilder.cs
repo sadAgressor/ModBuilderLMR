37b25839f39fc604a0f1bd506dd2ba04af3d287e
Mark Atolog <mentalistone@gmail.com>
2022-08-12 23:48:24 +0300

    add assets packing script

---
+++ ModBuilder.cs
@@ -0,0 +1,34 @@
+using System;
+using System.IO;
+using System.Linq;
+using UnityEngine;
+using UnityEditor;
+
+
+public class ModBuilder : MonoBehaviour
+{
+    [MenuItem("Mods/Build Mod")]
+    static void BuildMod()
+    {
+        AssetBundleBuild[] buildMap = new AssetBundleBuild[1];
+
+        var assetNames = AssetDatabase.GetAllAssetPaths().Where(s => s.StartsWith("Assets/") && !s.Contains("Editor") && !s.Contains(".meta")).ToArray();
+        var addressableNames = new string[assetNames.Length];
+
+        for (var i = 0; i < assetNames.Length; i++)
+        {
+            addressableNames[i] = assetNames[i].Replace("Assets/", "");
+        }
+
+        string modName = Application.productName;
+
+        buildMap[0].assetBundleName = $"{modName}.mod";
+        buildMap[0].assetNames = assetNames;
+        buildMap[0].addressableNames = addressableNames;
+
+        string modsFolderName = "Mods";
+        Directory.CreateDirectory(modsFolderName);
+
+        BuildPipeline.BuildAssetBundles(modsFolderName, buildMap, BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows);
+    }
+}
